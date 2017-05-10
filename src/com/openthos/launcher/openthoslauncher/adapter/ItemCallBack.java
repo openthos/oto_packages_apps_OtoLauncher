b28541efc67a6ef601cbfe8c929111461a3d0e5b
Wang Zhixu <potatomagic@163.com>
2017-05-10 10:57:36 +0000

    OtoLauncher: Because drag icon and long press to show menu dialog is conflicting, so romove the feature about drag icon
    Signed-off-by: Wang Zhixu <potatomagic@163.com>

--- src/com/openthos/launcher/openthoslauncher/adapter/ItemCallBack.java
+++ src/com/openthos/launcher/openthoslauncher/adapter/ItemCallBack.java
@@ -35,7 +35,7 @@ public class ItemCallBack extends ItemTouchHelper.Callback{
 
     @Override
     public boolean isLongPressDragEnabled() {
-        return true;
+        return false;
     }
 
     @Override
